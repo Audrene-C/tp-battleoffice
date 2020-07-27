7c730e5664f493513ab95a01a66232c843aaa22a
azerty <audrene.coatmeur@gmail.com>
2020-07-27 12:14:17 +0200

    entities created

--- src/Entity/Shipping.php
+++ src/Entity/Shipping.php
@@ -17,8 +17,110 @@ class Shipping
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adressLine1;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $adressLine2;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $zipcode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phone;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getAdressLine1(): ?string
+    {
+        return $this->adressLine1;
+    }
+
+    public function setAdressLine1(string $adressLine1): self
+    {
+        $this->adressLine1 = $adressLine1;
+
+        return $this;
+    }
+
+    public function getAdressLine2(): ?string
+    {
+        return $this->adressLine2;
+    }
+
+    public function setAdressLine2(?string $adressLine2): self
+    {
+        $this->adressLine2 = $adressLine2;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipcode(): ?int
+    {
+        return $this->zipcode;
+    }
+
+    public function setZipcode(int $zipcode): self
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
     }
 }
